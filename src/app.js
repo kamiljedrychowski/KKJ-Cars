71b64969251c1c11a3850311cfc1174eeeb14f22
Krzosa Karol <krzosa.karol@gmail.com>
2021-05-09 18:33:30 +0200

    Connect with database, Add models with proper connections

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
+const mongoose = require("mongoose")
 const express = require('express')
 const app = express()
 const port = 3000
+
+const mongo = "mongodb://127.0.0.1:27017"
+mongoose.connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "MongoDB connection error"))
+
+
 
 function main() {
   app.get('/', (req, res) => res.send('Hello world'))
   app.listen(port, () => console.log('http://localhost:' + port))
 }
+
 
 
 if (process.argv.length > 2) {
--- src/models.js
+++ src/models.js
@@ -1,40 +1,62 @@
+const { SchemaTypes } = require("mongoose")
+
 const Schema = require(mongoose).Schema
+/* 
+User to admin i pracownik
+  Klienta wykorzeniłem do osobnego modelu, nie jestem pewny czy dobrze to.
+    Klient ma auta
+      Auta mają odniesienia id do spotkań
+
+Spotkania
+  Spotkania mają pracownikow jako id
+  Spotkania mają serwisy
+    Serwisy mają części (Serwisy mają swoją cenę i części mają swoją cenę)
+      Serwisy maja id mechanika
+*/
 
 let User = new Schema({
-  username: {type: String},
-  email: {type: String},  // , format: email
-  id: {type: String},     // , format: uuid
+  contact: Contact,
+  username: { type: String },
+  email: { type: String },  // , format: email
+  id: { type: String },     // , format: uuid
+})
+let Customer = new Schema({
+  contact: Contact,
+  cars: [Car]
 })
 let Services = new Schema({
-  parts: [String],
-  worker_id: {type: String},  // , format: uuid
-  name: {type: String},
-  description: {type: String}
+  parts: [CarParts],
+  workerId: { type: String },  // , format: uuid
+  name: { type: String },
+  price: { type: Number }, // Service price
+  description: { type: String }
 })
 let Car = new Schema({
-  appointment_id: [String],  // format uuid,
-  VIN: {type: String},
-  licensePlate: {type: String},
-  model: {type: String},
-  brand: {type: String}
+  appointmentId: [SchemaTypes.ObjectId],  // format uuid,
+  VIN: { type: String },
+  licensePlate: { type: String },
+  model: { type: String },
+  brand: { type: String }
 })
 let CarParts = new Schema(
-    {price: {type: Number}, name: {type: String}, PID: {type: String}})
+  { price: { type: Number }, name: { type: String }, PID: { type: String } })
 let Contact = new Schema({
-  firstname: {type: String},
-  surname: {type: String},
-  email: {type: String},
-  address: {type: String},
-  phoneNumber: {type: String},
-  birthdate: {type: String},
-  gender: {type: String}
+  firstname: { type: String },
+  surname: { type: String },
+  email: { type: String },
+  address: { type: String },
+  phoneNumber: { type: String },
+  birthdate: { type: Date },
+  gender: { type: String }
 })
 let Appointment = new Schema({
-  services: [String],  // format uuid,
-  date: {type: String},
-  cost: {type: Number},
-  cancellationDate: {type: String},
-  deliveryDate: {type: String},
-  description: {type: String},
-  stars: {type: Number, min: 0, max: 5}
+  carId: SchemaTypes.ObjectId,
+  services: [Services],  // format uuid,
+  date: { type: Date },
+  // cost: { type: Number }, // To powinno wynikać z Services, chyba ze dodatkowa zaplata za caloksztalt tworczosci
+  cancellationDate: { type: Date },
+  deliveryDate: { type: Date },
+  description: { type: String },
+  stars: { type: Number, min: 0, max: 5 },
+  employee: [SchemaTypes.ObjectId]
 })
