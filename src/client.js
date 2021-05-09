0142764214788f0ef5d13000d5949dc73e960995
Krzosa Karol <krzosa.karol@gmail.com>
2021-05-09 21:13:46 +0200

    Add test client code

---
+++ src/client.js
@@ -0,0 +1,39 @@
+const http = require("http")
+
+function get(path) {
+    let options = {
+        host: 'localhost',
+        port: '3000',
+        method: 'GET',
+        path: path
+    }
+    http.request(options, function (response) {
+        let str = ''
+        response.on('data', function (chunk) { str += chunk })
+        response.on('end', function () {
+            console.log(options['method'], path, response.statusCode, str)
+        })
+    }).end()
+}
+
+function post(path, json) {
+    let options = {
+        host: 'localhost',
+        port: '3000',
+        method: 'POST',
+        path: path
+    }
+    let req = http.request(options, function (response) {
+        console.log()
+        let str = ''
+        response.on('data', function (chunk) { str += chunk })
+        response.on('end', function () {
+            console.log(options['method'], path, response.statusCode, str)
+        })
+    })
+    req.write(JSON.stringify(json))
+    req.end()
+}
+
+post('/v1/users', { username: "BOB", email: "BOB@gmail.com" })
+get('/v1/users')
