104a48cca819f3564ae39df2c20492e062216178
Krzosa Karol <krzosa.karol@gmail.com>
2021-05-10 12:19:43 +0200

    Add required to models

--- src/models.js
+++ src/models.js
@@ -15,49 +15,49 @@ Spotkania
       Serwisy maja id mechanika
 */
 const Contact = new Schema({
-  firstname: { type: String },
-  surname: { type: String },
-  email: { type: String },
-  address: { type: String },
-  phoneNumber: { type: String },
-  birthdate: { type: Date },
-  gender: { type: String }
+  firstname: { type: String, required: true },
+  surname: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  birthdate: { type: Date, required: true },
+  gender: { type: String, required: true }
 })
 const Car = new Schema({
   appointmentId: [SchemaTypes.ObjectId],  // format uuid,
-  VIN: { type: String },
-  licensePlate: { type: String },
-  model: { type: String },
-  brand: { type: String }
+  VIN: { type: String, required: true },
+  licensePlate: { type: String, required: true },
+  model: { type: String, required: true },
+  brand: { type: String, required: true }
 })
 const Customer = new Schema({
-  contact: Contact,
+  contact: { type: Contact, required: true },
   cars: [Car]
 })
 const CarParts = new Schema(
-  { price: { type: Number }, name: { type: String }, PID: { type: String } })
+  { price: { type: Number, required: true }, name: { type: String, required: true }, PID: { type: String, required: true } })
 const Services = new Schema({
   parts: [CarParts],
-  workerId: { type: String },  // , format: uuid
-  name: { type: String },
-  price: { type: Number }, // Service price
-  description: { type: String }
+  workerId: { type: String, required: true },  // , format: uuid
+  name: { type: String, required: true },
+  price: { type: Number, required: true }, // Service price
+  description: { type: String, required: false }
 })
 const User = new Schema({
-  contact: Contact,
-  username: { type: String },
-  email: { type: String },  // , format: email
-  type: { type: String, enum: ["ADMIN", "EMPLOYEE"] }
+  contact: { type: Contact, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },  // , format: email
+  type: { type: String, enum: ["ADMIN", "EMPLOYEE"], required: true }
 })
 const Appointment = new Schema({
-  carId: SchemaTypes.ObjectId,
+  carId: { type: SchemaTypes.ObjectId, required: true },
   services: [Services],  // format uuid,
-  date: { type: Date },
+  date: { type: Date, required: true },
   // cost: { type: Number }, // To powinno wynikać z Services, chyba ze dodatkowa zaplata za caloksztalt tworczosci
-  cancellationDate: { type: Date },
-  deliveryDate: { type: Date },
-  description: { type: String },
-  stars: { type: Number, min: 0, max: 5 },
+  cancellationDate: { type: Date, required: false },
+  deliveryDate: { type: Date, required: false },
+  description: { type: String, required: false },
+  stars: { type: Number, min: 0, max: 5, required: false },
   employee: [SchemaTypes.ObjectId]
 })
 
